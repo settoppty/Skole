cf6f493999234f845658334b8d72b87bebca119c
Afshin <settoppty@gmail.com>
2025-12-07 17:08:37 +0100

    Auto commit at 2025-12-07 17:08:36

---
+++ Kapittel 3/3F/listify/main/index.js
@@ -0,0 +1,9 @@
+const selectCategoryEl = document.querySelector("#sort");
+selectCategoryEl.addEventListener("change", showList)
+
+
+
+function showList(){
+    console.log("Er i Show List.");
+    
+}
